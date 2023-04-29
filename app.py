4c939c9df87a591df657b238f9a79369d86df709
Angyu WU <67306398+Angyu-Wu@users.noreply.github.com>
2023-04-29 13:14:53 -0700

    Add files via upload

--- app.py
+++ app.py
@@ -1,32 +1,52 @@
+
 from flask import Flask, request, jsonify
-import pickle
-import pandas as pd
 app = Flask(__name__)
 
 
-@app.route('/predict', methods=['GET'])
+@app.route('/getmsg/', methods=['GET'])
 def respond():
     # Retrieve the name from the url parameter /getmsg/?name=
-    vol_moving_avg = request.args.get("vol_moving_avg", None)
-    adj_close_rolling_med = request.args.get("adj_close_rolling_med", None)
-    # For debugging
-    #print(f"Received: {name}")
-    directory = rf'C:\Users\Wu\Desktop\book\stock dataset'
-    filename = directory+r'\finalized_model.sav'
-    loaded_model = pickle.load(open(filename, 'rb'))
-    d = {'vol_moving_avg': vol_moving_avg, 'adj_close_rolling_med': adj_close_rolling_med}
-    X = pd.DataFrame(data=d)
-    y_predicted = loaded_model.predict(X)
-    
+    name = request.args.get("name", None)
 
+    # For debugging
+    print(f"Received: {name}")
 
     response = {}
-    response["Prediction"] = f''+str(y_predicted[0])
+
     # Check if the user sent a name at all
+    if not name:
+        response["ERROR"] = "No name found. Please send a name."
+    # Check if the user entered a number
+    elif str(name).isdigit():
+        response["ERROR"] = "The name can't be numeric. Please send a string."
+    else:
+        response["MESSAGE"] = f"Welcome {name} to our awesome API!"
 
     # Return the response in json format
     return jsonify(response)
 
+
+@app.route('/post/', methods=['POST'])
+def post_something():
+    param = request.form.get('name')
+    print(param)
+    # You can add the test cases you made in the previous function, but in our case here you are just testing the POST functionality
+    if param:
+        return jsonify({
+            "Message": f"Welcome {name} to our awesome API!",
+            # Add this option to distinct the POST request
+            "METHOD": "POST"
+        })
+    else:
+        return jsonify({
+            "ERROR": "No name found. Please send a name."
+        })
+
+
+@app.route('/')
+def index():
+    # A welcome message to test our server
+    return "<h1>Welcome to our medium-greeting-api!</h1>"
 
 
 if __name__ == '__main__':
