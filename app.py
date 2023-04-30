649cb379e583517b5a99c404b13fcfde2ac9f2b7
Angyu WU <67306398+Angyu-Wu@users.noreply.github.com>
2023-04-29 20:54:11 -0700

    Add files via upload

--- app.py
+++ app.py
@@ -4,15 +4,6 @@ import pickle
 import pandas as pd
 app = Flask(__name__)
 
-#def model(vol_moving_avg,adj_close_rolling_med):
- #   directory = rf'C:\Users\Wu\Desktop\book\stock dataset'
-  #  filename = directory+r'\finalized_model.sav'
-    #loaded_model = pickle.load(open(filename, 'rb'))
-    #d = [{'vol_moving_avg': vol_moving_avg, 'adj_close_rolling_med': adj_close_rolling_med}]
-    #d = [{'vol_moving_avg': 1000, 'adj_close_rolling_med': 100}]
-    #X = pd.DataFrame(data=d)
-    #y_predicted = loaded_model.predict(X)
-    #return str(y_predicted[0])
 directory = rf'C:\Users\Wu\Desktop\book\stock dataset'
 filename = directory+r'\finalized_model.sav'
 loaded_model = joblib.load(open(filename, 'rb'))
@@ -22,28 +13,20 @@ def respond():
     # Retrieve the name from the url parameter /getmsg/?name=
     vol_moving_avg = request.args.get("vol_moving_avg",None)
     adj_close_rolling_med = request.args.get("adj_close_rolling_med", None)
-    #print(f"Received: {vol_moving_avg}")
-    #print(f"Received: {adj_close_rolling_med}")
-    # For debugging
-    #print(f"Received: {name}")
-    #directory = rf'C:\Users\Wu\Desktop\book\stock dataset'
-    #filename = directory+r'\finalized_model.sav'
-    #loaded_model = joblib.load(filename)
-    #d = {'vol_moving_avg': [vol_moving_avg], 'adj_close_rolling_med': [adj_close_rolling_med]}
+   
     d = [{'vol_moving_avg': vol_moving_avg, 'adj_close_rolling_med': adj_close_rolling_med}]
-    #d = [{'vol_moving_avg': 1000, 'adj_close_rolling_med': 100}]
+  
     X = pd.DataFrame(data=d)
     y_predicted = loaded_model.predict(X)
     result = f''+str(y_predicted[0])
-    #vol_moving_avg = str(vol_moving_avg)
-    #adj_close_rolling_med = str(adj_close_rolling_med)
+  
     
 
     
     response = {}
-    #response["Prediction"] = f''+str(y_predicted[0])
+    
     response["Prediction"] = f'vol_moving_avg is  {vol_moving_avg} and adj_close_rolling_med {adj_close_rolling_med} then result is {result}'
-    # Check if the user sent a name at all
+   
 
     # Return the response in json format
     return jsonify(response)
