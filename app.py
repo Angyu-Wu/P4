2ca0b58919a5f01f4b45d693c055230e1c0530e2
Angyu WU <67306398+Angyu-Wu@users.noreply.github.com>
2023-05-01 17:54:26 -0700

    Add files via upload

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import pickle
 import pandas as pd
 app = Flask(__name__)
 
+
 directory = rf'C:\Users\Wu\Desktop\book\stock dataset'
 filename = directory+r'\finalized_model.sav'
 loaded_model = joblib.load(open(filename, 'rb'))
@@ -15,18 +16,19 @@ def respond():
     adj_close_rolling_med = request.args.get("adj_close_rolling_med", None)
    
     d = [{'vol_moving_avg': vol_moving_avg, 'adj_close_rolling_med': adj_close_rolling_med}]
-  
+
     X = pd.DataFrame(data=d)
     y_predicted = loaded_model.predict(X)
     result = f''+str(y_predicted[0])
-  
+
     
 
     
     response = {}
+    response["Prediction"] = f'{result}'
     
-    response["Prediction"] = f'vol_moving_avg is  {vol_moving_avg} and adj_close_rolling_med {adj_close_rolling_med} then result is {result}'
-   
+    response["Result"] = f'vol_moving_avg is  {vol_moving_avg} and adj_close_rolling_med {adj_close_rolling_med} then result is {result}'
+    # Check if the user sent a name at all
 
     # Return the response in json format
     return jsonify(response)
